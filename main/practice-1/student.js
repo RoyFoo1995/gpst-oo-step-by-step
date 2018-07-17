5b2136889f1c94f1541e5cb7cd9f778665ab852a
yifu <yifu@thoughtworks.com>
2018-07-17 17:39:22 +0800

    [fuyi] finish the practice-1

--- main/practice-1/student.js
+++ main/practice-1/student.js
@@ -1,3 +1,14 @@
 const Person = require('./person');
 
-module.exports = class Student {}
+class Student extends Person {
+    constructor(name, age, clazz) {
+        super(name, age);
+        this.clazz = clazz;
+    }
+
+    introduce() {
+        return `My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.clazz}.`
+    }
+};
+
+module.exports = Student;
